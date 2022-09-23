99aa4700820586e42333873f56aa14457c7e90e5
junghyun89 <wjdgus8970@gmail.com>
2022-09-23 23:44:08 +0900

    ing

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,5 +1,6 @@
 import http from 'http';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 import express from 'express';
 
 const app = express();
@@ -13,19 +14,53 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log('listening');
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+instrument(wsServer, {
+  auth: false,
+});
+
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
 
 wsServer.on('connection', (socket) => {
   socket['nickname'] = 'Anonymous';
+  socket.onAny((e) => {
+    // console.log(wsServer.sockets.adapter);
+  });
   socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit('welcome', socket.nickname);
+    socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('disconnecting', () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit('bye', socket.nickname)
+      socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
     );
+  });
+  socket.on('disconnect', () => {
+    wsServer.sockets.emit('room_change', publicRooms());
   });
   socket.on('new_message', (msg, room, done) => {
     socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
@@ -33,27 +68,5 @@ wsServer.on('connection', (socket) => {
   });
   socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
-
-// const sockets = [];
-
-// wss.on('connection', (socket) => {
-//   sockets.push(socket);
-//   socket['nickname'] = 'Anonymous';
-//   console.log('Connected to Browser');
-//   socket.on('close', () => {
-//     console.log('Disconnected from the Browser');
-//   });
-//   socket.on('message', (msg) => {
-//     const message = JSON.parse(msg);
-//     switch (message.type) {
-//       case 'new_message':
-//         sockets.forEach((aSocket) =>
-//           aSocket.send(`${socket.nickname}: ${message.payload}`)
-//         );
-//       case 'nickname':
-//         socket['nickname'] = message.payload;
-//     }
-//   });
-// });
 
 httpServer.listen(3000, handleListen);
