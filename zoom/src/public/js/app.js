1b6c2c9311907bc0406ccff896e1270496a4d02a
junghyun89 <wjdgus8970@gmail.com>
2022-09-25 17:29:22 +0900

    YAY

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -5,6 +5,8 @@ const muteBtn = document.querySelector('#mute');
 const cameraBtn = document.querySelector('#camera');
 const camerasSelect = document.querySelector('#cameras');
 const call = document.querySelector('#call');
+const chat = document.querySelector('#chat');
+const chatList = document.querySelector('#chatList');
 
 call.hidden = true;
 
@@ -12,6 +14,8 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -83,6 +87,13 @@ function handleCameraClick() {
 async function handleCameraChange(e) {
   if (camerasSelect.value === 'Select Camera') return;
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === 'video');
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteBtn.addEventListener('click', handleMuteClick);
@@ -92,22 +103,103 @@ camerasSelect.addEventListener('input', handleCameraChange);
 const welcome = document.querySelector('#welcome');
 const welcomeForm = welcome.querySelector('form');
 
-function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
-function handleRoomSubmit(e) {
+async function handleRoomSubmit(e) {
   e.preventDefault();
   const input = welcomeForm.querySelector('input');
-  socket.emit('join_room', input.value, startMedia);
+  await initCall();
+  socket.emit('join_room', input.value);
   roomName = input.value;
   input.value = '';
 }
 
+function addMessage(message) {
+  const li = document.createElement('li');
+  li.innerText = message;
+  chatList.append(li);
+}
+
 welcomeForm.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-  console.log('joined');
+socket.on('welcome', async () => {
+  myDataChannel = myPeerConnection.createDataChannel('chat');
+  myDataChannel.addEventListener('message', (e) => {
+    addMessage(e.data);
+  });
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log('sent the offer');
+  socket.emit('offer', offer, roomName);
 });
+
+socket.on('offer', async (offer) => {
+  myPeerConnection.addEventListener('datachannel', (e) => {
+    myDataChannel = e.channel;
+    myDataChannel.addEventListener('message', (e) => {
+      addMessage(e.data);
+    });
+  });
+  console.log('received the offer');
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit('answer', answer, roomName);
+  console.log('sent the answer');
+});
+
+socket.on('answer', (answer) => {
+  console.log('received the answer');
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on('ice', (ice) => {
+  console.log('recieved candidate');
+  myPeerConnection.addIceCandidate(ice);
+});
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener('icecandidate', handleIce);
+  myPeerConnection.addEventListener('addstream', handleAddStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log('sent candidate');
+  socket.emit('ice', data.candidate, roomName);
+}
+
+function handleAddStream(data) {
+  const peerFace = document.querySelector('#peerFace');
+  peerFace.srcObject = data.stream;
+}
+
+function handleMessageSubmit(e) {
+  e.preventDefault();
+  const input = chat.querySelector('input');
+  addMessage(input.value);
+  myDataChannel.send(input.value);
+  input.value = '';
+}
+
+chat.addEventListener('submit', handleMessageSubmit);
