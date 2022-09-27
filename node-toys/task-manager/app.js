0acfc62a9b37e2d6031c242e85591ebe41da8000
junghyun89 <wjdgus8970@gmail.com>
2022-09-28 08:54:39 +0900

    edit directory

--- node-toys/task-manager/app.js
+++ node-toys/task-manager/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const tasks = require('./routes/tasks');
-const connectDB = require('./db/connect');
+const tasks = require('./src/routes/tasks');
+const connectDB = require('./src/db/connect');
 require('dotenv').config();
-const notFound = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFound = require('./src/middleware/not-found');
+const errorHandlerMiddleware = require('./src/middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
