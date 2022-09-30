c2464c8f3e75e2e2df10662a46fe7071de07c693
junghyun89 <wjdgus8970@gmail.com>
2022-09-30 23:30:01 +0900

    YAY

--- node-toys/jobs-api/app.js
+++ node-toys/jobs-api/app.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
 // connectDB
 const connectDB = require('./src/db/connect');
+const authenticateUser = require('./src/middleware/auth');
 
 //routers
 const authRouter = require('./src/routes/auth');
@@ -14,13 +27,28 @@ const jobsRouter = require('./src/routes/jobs');
 const notFoundMiddleware = require('./src/middleware/not-found');
 const errorHandlerMiddleware = require('./src/middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // extra packages
 
+app.get('/', (req, res) => {
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
