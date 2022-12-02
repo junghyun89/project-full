38cef019d84d93fdff3415f74fdd16f932cf1ffe
junghyun89 <wjdgus8970@gmail.com>
2022-12-02 22:08:40 +0900

    server done

--- e-commerce/client/src/App.jsx
+++ e-commerce/client/src/App.jsx
@@ -4,9 +4,57 @@ import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
 
 const App = () => {
-  return <Cart />;
+  const user = true;
+  const NoUserRoute = ({ children }) => {
+    if (user) return <Navigate to="/" />;
+    return children;
+  };
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/login',
+      element: (
+        <NoUserRoute>
+          <Login />
+        </NoUserRoute>
+      ),
+    },
+    {
+      path: '/register',
+      element: (
+        <NoUserRoute>
+          <Register />
+        </NoUserRoute>
+      ),
+    },
+    {
+      path: '/cart',
+      element: <Cart />,
+    },
+    {
+      path: '/products/:category',
+      element: <ProductList />,
+    },
+    {
+      path: '/product/:id',
+      element: <Product />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 };
 
 export default App;
